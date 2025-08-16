131f65360975e02dfab1d499c2c1d840e17d0754
Rida MEFTAH <rmeftah@jems-group.com>
2025-08-16 04:16:25 +0200

    ok

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Démarrer Spark Master
-/opt/bitnami/spark/sbin/start-master.sh
+/opt/bitnami/spark/sbin/start-master.sh --host 0.0.0.0
 
 # Lancer Jupyter Notebook
 jupyter notebook --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 
 # Garder le conteneur vivant (optionnel si Jupyter reste au premier plan)
-tail -f /dev/null
+#tail -f /dev/null
